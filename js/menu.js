159fc6c59dde7b8bd52b4744d4905a1c785b4b43
UserIryna28 <Ltdcom@ukr.net>
2023-02-16 17:39:59 +0200

    Update home-work

--- js/menu.js
+++ js/menu.js
@@ -5,8 +5,8 @@
     menu: document.querySelector("[data-menu]"),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggle-Menu);
-  refs.closeMenuBtn.addEventListener("click", toggle-Menu);
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
